b9bc23597b030846abbde2d43108192d27119b64
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-10-12 14:51:28 -0300

    feat: user cpf validation before creation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
@@ -69,10 +70,41 @@ class Users(Resource):
 
 
 class User(Resource):
+    def validate_cpf(self, cpf):
+        if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+            return False
+
+        numbers = [int(digit) for digit in cpf if digit.isdigit()]
+
+        if len(numbers) != 11 or len(set(numbers)) == 1:
+            return False
+
+        # Validação do primeiro dígito verificador:
+        sum_of_products = sum(
+            a*b for a, b in zip(numbers[0:9], range(10, 1, -1))
+        )
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[9] != expected_digit:
+            return False
+
+        # Validação do segundo dígito verificador:
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[10] != expected_digit:
+            return False
+
+        return True
+
     def post(self):
         data = _user_parser.parse_args()
-        UserModel(**data).save()
-        return data
+
+        if not self.validate_cpf(data["cpf"]):
+            return {"message": "CPF is invalid!"}, 300
+
+        response = UserModel(**data).save()
+        return {
+            "message": f"User {response.first_name} {response.last_name} successfully created!"
+        }, 201
 
     def get(self, cpf):
         return {"message": f"user and {cpf}"}
