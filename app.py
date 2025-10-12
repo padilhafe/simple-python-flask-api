ff6d6604e788c75bb6a5c463c9e2fce84d64cbe7
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-10-12 13:35:05 -0300

    feat: added reqparse feature to user post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 
 
@@ -15,6 +15,39 @@ app.config['MONGODB_SETTINGS'] = {
     'password': 'admin'
 }
 db = MongoEngine(app)
+
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument(
+    'first_name',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'last_name',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'cpf',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'email',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parser.add_argument(
+    'birth_date',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
 
 
 class UserModel(db.Document):
@@ -37,7 +70,9 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {"message": "user"}
+        data = _user_parser.parse_args()
+        UserModel(**data).save()
+        return data
 
     def get(self, cpf):
         return {"message": f"user and {cpf}"}
