ba54e97dc45d122e96f3309e882bf10141659d52
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-10-18 09:52:19 -0300

    fix: configure MongoDB settings using environment variables

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
+
 class DevConfig():
     MONGODB_SETTINGS = {
-        'db': 'users',
-        'host': 'mongo',
-        'port': 27017,
-        'username': 'admin',
-        'password': 'admin'
+        'db': os.getenv('MONGODB_DB'),
+        'host': os.getenv('MONGODB_HOST'),
+        'port': int(os.getenv('MONGODB_PORT', 27017)),
+        'username': os.getenv('MONGODB_USERNAME'),
+        'password': os.getenv('MONGODB_PASSWORD'),
     }
